04a11724f7d392d4d43a44bda8110fc1af11235e
Guy Powell <guy@PinaVida.co.uk>
2025-08-02 19:33:08 +0100

    Fix code formatting in BaseDataObject
    Applied dotnet format to fix whitespace formatting issues in BaseDataObject.cs

--- Domain/BaseDataObject.cs
+++ Domain/BaseDataObject.cs
@@ -9,12 +9,12 @@ namespace Domain
         public Guid Id { get; set; }
         public DateTimeOffset DateCreated { get; set; }
         public DateTimeOffset DateUpdated { get; set; }
-        
+
         [BsonElement("version")]
         [BsonRequired]
         [Required]
         public long Version { get; set; }
-        
+
         public BaseDataObject()
         {
             Version = 1;
