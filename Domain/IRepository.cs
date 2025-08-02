e371fe13b2af8f32f1137d68e084635636b7d4e9
Guy Powell <guy@PinaVida.co.uk>
2025-08-02 17:10:24 +0100

    Add XML documentation to IRepository.PatchAsync method
    - Added comprehensive XML documentation comments to the PatchAsync method in IRepository.cs
    - Documentation describes the method purpose, parameters (id, updates, propertyNames), and return value
    - Ensures interface documentation matches the implementation in BaseMongoRepository
    - Clarifies that when propertyNames array is empty, all properties from updates object are considered

--- Domain/IRepository.cs
+++ Domain/IRepository.cs
@@ -11,6 +11,13 @@ namespace Domain
         Task<T> UpdateAsync(T entity);
         Task InsertManyAsync(IEnumerable<T> entities);
         Task DeleteAsync(Guid id);
+        /// <summary>
+        /// Partially updates an entity with specified updates and property names
+        /// </summary>
+        /// <param name="id">The unique identifier of the entity to update</param>
+        /// <param name="updates">An object containing the fields to update</param>
+        /// <param name="propertyNames">Array of property names to update. If empty, all properties from the updates object will be considered</param>
+        /// <returns>A task representing the asynchronous patch operation</returns>
         Task PatchAsync(Guid id, object updates, params string[] propertyNames);
     }
 }
