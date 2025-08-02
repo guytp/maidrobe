577155320addb70d0fa4b6386da25cff69d21dc2
Guy Powell <guy@PinaVida.co.uk>
2025-08-02 14:18:39 +0100

    Fix compilation errors and improve code quality
    - Implement all missing IRepository<T> interface methods in BaseMongoRepository
    - Fix UpdateAsync return type to match interface (Task<T> instead of Task)
    - Add support for both Guid and string ID operations
    - Fix DateTime to DateTimeOffset for consistency with BaseDataObject
    - Add explicit interface implementation to resolve method signature conflicts
    - Add necessary using statements for collections and LINQ

--- Infrastructure.Data/BaseMongoRepository.cs
+++ Infrastructure.Data/BaseMongoRepository.cs
@@ -1,6 +1,8 @@
 using Domain;
 using MongoDB.Driver;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data
@@ -16,7 +18,8 @@ namespace Infrastructure.Data
             _collection = _database.GetCollection<T>(typeof(T).Name);
         }
 
-        public async Task<T> GetByIdAsync(string id)
+        // IRepository<T> implementations using Guid
+        public async Task<T> GetByIdAsync(Guid id)
         {
             var filter = Builders<T>.Filter.Eq(x => x.Id, id);
             return await _collection.Find(filter).FirstOrDefaultAsync();
@@ -24,47 +27,108 @@ namespace Infrastructure.Data
 
         public async Task<T> CreateAsync(T entity)
         {
-            entity.DateCreated = DateTime.UtcNow;
-            entity.DateUpdated = DateTime.UtcNow;
+            entity.DateCreated = DateTimeOffset.UtcNow;
+            entity.DateUpdated = DateTimeOffset.UtcNow;
             await _collection.InsertOneAsync(entity);
             return entity;
         }
 
-        public async Task InsertAsync(T entity)
+        public async Task InsertAsync(IEnumerable<T> entities)
         {
-            entity.DateCreated = DateTime.UtcNow;
-            entity.DateUpdated = DateTime.UtcNow;
-            await _collection.InsertOneAsync(entity);
+            var entitiesList = entities.ToList();
+            foreach (var entity in entitiesList)
+            {
+                entity.DateCreated = DateTimeOffset.UtcNow;
+                entity.DateUpdated = DateTimeOffset.UtcNow;
+            }
+            await _collection.InsertManyAsync(entitiesList);
         }
 
-        public async Task UpdateAsync(T entity)
+        public async Task<T> UpdateAsync(T entity)
         {
-            entity.DateUpdated = DateTime.UtcNow;
+            entity.DateUpdated = DateTimeOffset.UtcNow;
             var filter = Builders<T>.Filter.Eq(x => x.Id, entity.Id);
             await _collection.ReplaceOneAsync(filter, entity);
+            return entity;
         }
 
-        public async Task PatchAsync(string id, object updates)
+        public async Task DeleteAsync(Guid id)
         {
             var filter = Builders<T>.Filter.Eq(x => x.Id, id);
-            var updateDefinition = Builders<T>.Update.Set(x => x.DateUpdated, DateTime.UtcNow);
+            await _collection.DeleteOneAsync(filter);
+        }
+
+        public async Task PatchAsync(Guid id, string[] propertyNames)
+        {
+            // For the string[] version, we need to get the entity first to extract property values
+            var entity = await GetByIdAsync(id);
+            if (entity == null) return;
+
+            var filter = Builders<T>.Filter.Eq(x => x.Id, id);
+            var updateDefinition = Builders<T>.Update.Set(x => x.DateUpdated, DateTimeOffset.UtcNow);
             
-            foreach (var property in updates.GetType().GetProperties())
+            var entityType = typeof(T);
+            foreach (var propertyName in propertyNames)
             {
-                var value = property.GetValue(updates);
-                if (value != null)
+                var property = entityType.GetProperty(propertyName);
+                if (property != null)
                 {
-                    updateDefinition = updateDefinition.Set(property.Name, value);
+                    var value = property.GetValue(entity);
+                    updateDefinition = updateDefinition.Set(propertyName, value);
                 }
             }
             
             await _collection.UpdateOneAsync(filter, updateDefinition);
         }
 
+        // MongoDB-specific overloads using string IDs
+        public async Task<T?> GetByIdAsync(string id)
+        {
+            if (Guid.TryParse(id, out var guidId))
+            {
+                return await GetByIdAsync(guidId);
+            }
+            return null;
+        }
+
+        public async Task InsertAsync(T entity)
+        {
+            entity.DateCreated = DateTimeOffset.UtcNow;
+            entity.DateUpdated = DateTimeOffset.UtcNow;
+            await _collection.InsertOneAsync(entity);
+        }
+
+        Task<T> IMongoRepository<T>.UpdateAsync(T entity)
+        {
+            return UpdateAsync(entity);
+        }
+
+        public async Task PatchAsync(string id, object updates)
+        {
+            if (Guid.TryParse(id, out var guidId))
+            {
+                var filter = Builders<T>.Filter.Eq(x => x.Id, guidId);
+                var updateDefinition = Builders<T>.Update.Set(x => x.DateUpdated, DateTimeOffset.UtcNow);
+                
+                foreach (var property in updates.GetType().GetProperties())
+                {
+                    var value = property.GetValue(updates);
+                    if (value != null)
+                    {
+                        updateDefinition = updateDefinition.Set(property.Name, value);
+                    }
+                }
+                
+                await _collection.UpdateOneAsync(filter, updateDefinition);
+            }
+        }
+
         public async Task DeleteAsync(string id)
         {
-            var filter = Builders<T>.Filter.Eq(x => x.Id, id);
-            await _collection.DeleteOneAsync(filter);
+            if (Guid.TryParse(id, out var guidId))
+            {
+                await DeleteAsync(guidId);
+            }
         }
     }
 }
--- Infrastructure.Data/IMongoRepository.cs
+++ Infrastructure.Data/IMongoRepository.cs
@@ -1,14 +1,15 @@
 using Domain;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data
 {
     public interface IMongoRepository<T> : IRepository<T> where T : BaseDataObject
     {
-        Task<T> GetByIdAsync(string id);
-        Task<T> CreateAsync(T entity);
+        // MongoDB-specific overloads using string IDs
+        Task<T?> GetByIdAsync(string id);
         Task InsertAsync(T entity);
-        Task UpdateAsync(T entity);
+        new Task<T> UpdateAsync(T entity);
         Task PatchAsync(string id, object updates);
         Task DeleteAsync(string id);
     }
